d97536953eb4a1797a5436c04dcd4b77b0e6c964
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-11-02 10:34:05 -0600

    Added styling to exercise details

--- src/components/ExerciseDetails.js
+++ src/components/ExerciseDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 
-import { Typography, Stack, Button } from '@mui/material';
+import { Typography, Stack, Button, Box } from '@mui/material';
 
 const ExerciseDetails = ({ exercises }) => {
   const { id } = useParams();
@@ -10,31 +10,71 @@ const ExerciseDetails = ({ exercises }) => {
   const exercise = exercises.find((exercise) => exercise.id === id);
 
   return (
-    <Stack>
-      <Typography>{exercise.name}</Typography>
-      <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
-      <Typography>Body Part: {exercise.bodyPart}</Typography>
-      <Typography>Target Muscles: {exercise.target}</Typography>
-      <Typography>Secondary Muscles: {exercise.secondaryMuscles}</Typography>
-      <Typography>Instruction: {exercise.instruction}</Typography>
-      <Typography>Equipment: {exercise.equipment}</Typography>
-      <Link to={`/`}>
-        <Button
-          sx={{
-            marginTop: '35px',
-            background: '#a53860',
-            ml: '21px',
-            color: '#fff',
-            fontSize: '14px',
-            borderRadius: '20px',
-            textTransform: 'capitalize',
-            cursor: 'pointer',
-          }}
-        >
-          Back
-        </Button>
-      </Link>
-    </Stack>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Stack className='exercise-card'>
+        <Typography fontWeight='bold' textTransform='capitalize'>
+          {exercise.name}
+        </Typography>
+        <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
+        <Typography padding={'5px'} textTransform='capitalize'>
+          <span style={{ fontWeight: 'bold', color: '#8e2d53' }}>
+            Body Part:{' '}
+          </span>
+          {exercise.bodyPart}
+        </Typography>
+        <Typography padding={'5px'} textTransform='capitalize'>
+          <span style={{ fontWeight: 'bold', color: '#8e2d53' }}>
+            Target Muscles:{' '}
+          </span>
+          {exercise.target}
+        </Typography>
+        <Typography padding={'5px'} textTransform='capitalize'>
+          <span style={{ fontWeight: 'bold', color: '#8e2d53' }}>
+            Secondary Muscles:{' '}
+          </span>
+          {exercise.secondaryMuscles}
+        </Typography>
+        <Typography padding={'5px'} textTransform='capitalize'>
+          <span style={{ fontWeight: 'bold', color: '#8e2d53' }}>
+            Equipment:{' '}
+          </span>
+          {exercise.equipment}
+        </Typography>
+        <Typography padding={'5px'}>
+          <span style={{ fontWeight: 'bold', color: '#8e2d53' }}>
+            Instructions:
+          </span>
+          <ol>
+            {exercise.instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+        </Typography>
+        <Link to={`/exercises`}>
+          <Button
+            sx={{
+              marginTop: '35px',
+              background: '#a53860',
+              ml: '21px',
+              color: '#fff',
+              fontSize: '18px',
+              borderRadius: '20px',
+              textTransform: 'capitalize',
+              cursor: 'pointer',
+              padding: '10px 20px',
+            }}
+          >
+            Back
+          </Button>
+        </Link>
+      </Stack>
+    </Box>
   );
 };
 
